c3c2f4b11f472668e9bd81987d54309283f1c59c
Ethan Degenhardt <ethandeg1996@gmail.com>
2021-10-30 01:37:07 -0400

    updated sql schema and data

--- gym_tracker.sql
+++ gym_tracker.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2021 at 04:54 PM
+-- Generation Time: Oct 30, 2021 at 07:36 AM
 -- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,12 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_firstname` varchar(25) NOT NULL,
+  `user_lastname` varchar(25) NOT NULL,
+  `user_weight` float DEFAULT NULL,
+  `user_email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `user_firstname`, `user_lastname`, `user_weight`, `user_email`) VALUES
+(1, 'admin', 'admin', 'Ethan', 'Degenhardt', 200, 'admin@gmail.com'),
+(2, 'user', 'user', 'first', 'last', 255, 'user@gmail.com'),
+(3, 'john_wick', 'johnwick', 'john', 'wick', 0, 'john@gmail.com'),
+(4, 'bravo', 'bravo', 'bro', 'vo', NULL, 'bro@gmail.com'),
+(5, 'garyneelz', 'imgary', 'gary', 'neeley', NULL, 'gary@gmail.com'),
+(10, 'test', 'test', 'test', 'test', NULL, 'test@gmail.com'),
+(11, 'blargh', 'test', 'blarch', 'jalsdf', NULL, 'blargh@gmail.com');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `workouts`
 --
 
 CREATE TABLE `workouts` (
   `workout_id` int(11) NOT NULL,
-  `workout_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `workout_date` date NOT NULL DEFAULT current_timestamp(),
   `workout_length` float NOT NULL,
   `workout_muscle_group` varchar(25) NOT NULL,
   `workout_gym` varchar(59) DEFAULT NULL
@@ -40,13 +69,22 @@ CREATE TABLE `workouts` (
 --
 
 INSERT INTO `workouts` (`workout_id`, `workout_date`, `workout_length`, `workout_muscle_group`, `workout_gym`) VALUES
-(1, '2021-10-28 05:00:00', 55, 'Back and Biceps', 'Aliro'),
-(2, '2021-10-28 05:00:00', 120, 'Chest and Triceps', 'Planet Fitness'),
-(3, '2021-10-29 02:15:14', 20, 'test', 'test');
+(1, '2021-10-28', 55, 'Back and Biceps', 'Aliro'),
+(2, '2021-10-28', 120, 'Chest and Triceps', 'Planet Fitness'),
+(3, '2021-10-28', 20, 'test', 'test'),
+(4, '2021-10-30', 12, 'test1', 'test1'),
+(5, '2021-10-04', 100, 'legs', 'planet');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`,`username`),
+  ADD UNIQUE KEY `user_email` (`user_email`);
 
 --
 -- Indexes for table `workouts`
@@ -59,10 +97,16 @@ ALTER TABLE `workouts`
 --
 
 --
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `workouts`
 --
 ALTER TABLE `workouts`
-  MODIFY `workout_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `workout_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
