a328a2ec541c5f1b96a15ed99312b1c529484241
Ethan Degenhardt <ethandeg1996@gmail.com>
2022-03-04 21:43:00 -0500

    this is a test

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+echo "<h1>Hello, world!</h1>"; 
+exit;
+?>
 <?php include "./includes/header.php"; ?>
 <?php include PROJECT_ROOT_PATH . "/includes/navigation.php"; 
 //We grab user variables here, as well as authorization if user is logged in
